d40ec170cae9671bd530b1e083cb00bec49c4c91
vanta blanta <vantablanta@gmail.com>
2022-05-31 16:41:51 +0300

    CRISPY METHOD OF STYLING FORMS

--- formsapp/forms.py
+++ formsapp/forms.py
@@ -8,10 +8,5 @@ class RandomForm(ModelForm):
         class Meta():
             model= RandomModel
             fields =  ['name', 'email', 'message']
-            widgets = {
-                'name' : forms.TextInput(attrs={'class':'form-control blue', 'placeholder':'Hello', }),
-                'email' : forms.EmailInput(attrs={'class':'form-control'}),
-                'message' : forms.Textarea(attrs={'class':'form-control'}),
-            }
-
+            
 
--- formsproject/settings.py
+++ formsproject/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'formsapp.apps.FormsappConfig', 
     'bootstrap4', 
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
